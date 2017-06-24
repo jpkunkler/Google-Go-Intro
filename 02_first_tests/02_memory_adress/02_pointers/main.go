4dd2d08e7a1637838fede058e422f505a3128a4b
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-06-24 23:24:24 +0200

    memory addresses and pointers

---
+++ 02_first_tests/02_memory_adress/02_pointers/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 42
+
+	fmt.Println(a)
+	fmt.Println(&a)
+
+	var b *int = &a // b is of type "int pointer" -- the * is part of the type!!
+
+	fmt.Println(b)
+	fmt.Println(*b) // *b means de-referencing memory address
+
+	*b = 44 // the value at this address, change it to 44
+	fmt.Println(a)
+}
