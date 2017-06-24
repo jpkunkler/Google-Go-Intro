0bb65acd3196da051abc12b6a4d1bf27f2a4083d
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-06-24 22:42:37 +0200

    updated tests

--- 02_first_tests/01_constants/02_multiple_initialization/main.go
+++ 02_first_tests/01_constants/02_multiple_initialization/main.go
@@ -5,9 +5,11 @@ import "fmt"
 const (
 	pi       = 3.14
 	language = "Go"
+	name string = "Jan"
 )
 
 func main() {
 	fmt.Println(pi)
 	fmt.Println(language)
+	fmt.Printf("%T\t%q",name, name)
 }
---
+++ 02_first_tests/01_constants/03_iota/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const (
+	_  = iota // throw away 0
+	KB = 1 << (iota * 10) // byteshift to the left: 1 << (1 * 10)
+	MB = 1 << (iota * 10) // 1 << (2*10)
+	GB = 1 << (iota * 10) // 1 << (3 * 10)
+)
+
+func main() {
+	fmt.Printf("%b ", KB)
+	fmt.Printf("%d\t\n", KB)
+	fmt.Printf("%b\t", MB)
+	fmt.Printf("%d\t\n", MB)
+	
+}
