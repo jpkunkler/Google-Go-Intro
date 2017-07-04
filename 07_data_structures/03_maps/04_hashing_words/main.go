c45e16d97f7482685f699eabaa7898eef2ee1bc9
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-04 16:20:51 +0200

    extended map example

---
+++ 07_data_structures/03_maps/04_hashing_words/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	words := make(map[string]string)
+
+	// scan our result into buffer
+	sc := bufio.NewScanner(res.Body)
+	sc.Split(bufio.ScanWords) // split buffer by words
+
+	for sc.Scan() { // for every word we scanned
+		words[sc.Text()] = "" //add entry to our map
+	}
+	if err := sc.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+
+	i := 0
+	for k, _ := range words { //we only need the key since our values are all empty ""
+		fmt.Println(k)
+		if i == 200 {
+			break
+		}
+		i++
+	}
+}
