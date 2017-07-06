fc739d62ea4bd72b5ef77b1c1e953a845db637e9
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-06 12:40:10 +0200

    fixed typo

---
+++ 08_interfaces/01_introduction/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// define our type Square which takes the length of a side
+type Square struct {
+	side float64
+}
+
+// any type that uses the area() float64 method will be a shape
+type Shape interface {
+	area() float64
+}
+
+// attach method to our type Square
+func (z Square) area() float64 {
+	return z.side * z.side
+}
+
+func info(z Shape) {
+	fmt.Println(z)
+	fmt.Println(z.area())
+}
+func main() {
+	s := Square{10}
+	info(s)
+}
