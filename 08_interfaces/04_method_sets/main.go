6d11561e603834f988eb0d44f7b5174418cda2a2
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-07 13:15:30 +0200

    added method sets example

---
+++ 08_interfaces/04_method_sets/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	side float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type shape interface {
+	area() float64
+}
+
+// if we have a pointer receiver here, we have to pass in an address
+func (s *square) area() float64 {
+	return s.side * s.side
+}
+
+// if we only take in a shape, we can pass either an address or a value!
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func main() {
+	s := square{5}
+	c := circle{5}
+	info(&s)
+	info(c)
+	info(&c)
+}
+
+/*
+
+RECEIVER	VALUE
+-----------------------
+(t T)		T and *T
+(t *T)		*T
+
+*/
