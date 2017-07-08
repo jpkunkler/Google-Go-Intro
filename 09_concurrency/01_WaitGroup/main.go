b7c7271c547025f97882b26dbfbf795bdf122e70
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-08 14:50:17 +0200

    added concurrency & WaitGroup example

---
+++ 09_concurrency/01_WaitGroup/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// assign and instantiate a WaitGroup
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2) // add 2 to WaitGroups since we run 2 functions
+
+	// start goroutines
+	go foo()
+	go bar()
+	wg.Wait() // wait until all functions are done
+}
+
+func foo() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("Foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < 100; i++ {
+		fmt.Println("Bar:", i)
+	}
+	wg.Done()
+}
