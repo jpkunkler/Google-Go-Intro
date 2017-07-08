494694c4b004e283a2c47b640c658b5f418afec9
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-08 16:39:09 +0200

    added mutex example to prevent data race

---
+++ 09_concurrency/04_mutex/main.go
@@ -0,0 +1,38 @@
+/*
+MUTEX stands for mutual exclusion object
+ */
+package main
+
+import (
+	"sync"
+	"fmt"
+)
+
+var wg sync.WaitGroup
+var counter int
+var mutex sync.Mutex
+
+func main() {
+	wg.Add(2)
+
+	incrementor("Foo: ")
+	incrementor("Bar: ")
+
+	wg.Wait()
+	fmt.Println("Counter: ", counter)
+}
+
+func incrementor(s string) {
+	for i := 0; i < 20; i++ {
+		// lock the process so that no other thread can use it
+		mutex.Lock()
+
+		// do the work here while only we have access
+		counter++
+		fmt.Println(s, i, "Counter: ", counter)
+
+		// unlock it so others get to access it again
+		mutex.Unlock()
+	}
+	wg.Done()
+}
