b4a033f849a83670f895fbdf8bfa2572fbaa113f
Jan Pascal Kunkler <pascal.kunkler@gmail.com>
2017-07-12 17:54:11 +0200

    added complete fan out fan in example

---
+++ 10_channels/10_fan_in_fan_out/02_fan_out-fan_in/main.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+import "sync"
+
+func main() {
+	in := gen(2, 3)
+
+	// FAN OUT
+	// distribute the sq work across two goroutines that both read from them
+	c1 := sq(in)
+	c2 := sq(in)
+
+	// FAN IN
+	// Consume the merged output from multiple channels
+	for n := range merge(c1, c2) {
+		fmt.Println(n)
+	}
+}
+
+// Generate and populate a channel with numbers
+func gen(nums ...int) chan int {
+	out := make(chan int)
+	go func() {
+		for _, num := range nums {
+			out <- num
+		}
+		close(out)
+	}()
+	return out
+}
+
+// Square input values and return a channel
+func sq(input chan int) chan int {
+	out := make(chan int)
+	go func() {
+		for n := range input {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...chan int) chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go func(ch chan int) {
+			for n := range ch {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+
+	// Wait until every goroutine is done, then close the channel to prevent deadlock
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
